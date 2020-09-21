e3e87cfb6d317de52e120f7ee216a5c960004b1c
Alex <alex@q-83.com>
2020-09-21 23:23:37 +1000

    first commit

---
+++ src/main/java/com/alex/java/JavaApplication.java
@@ -0,0 +1,18 @@
+package com.alex.java;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class JavaApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(JavaApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/alex/java/JavaApplicationTests.java
@@ -0,0 +1,25 @@
+package com.alex.java;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class JavaApplicationTests {
+
+    // private final Logger logger = LoggerFactory.getLogger(JavaApplicationTests.class);
+
+    @Test
+    void logging() {
+        log.warn("this is a warning ...");
+        log.info("info level ...");
+        log.error("this is an error");
+        log.debug("debug level...");
+        log.trace("this is a trace");
+
+    }
+
+}
